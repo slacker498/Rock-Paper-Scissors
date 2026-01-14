d9b80d46371323f60fefb49c9801f20e5803902c
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-14 19:01:19 +0000

    Resolve score increment issue again by moving playRound into playGame

--- script.js
+++ script.js
@@ -17,58 +17,55 @@ function getHumanChoice() {
     return prompt("Enter one of the following values (“rock”, “paper” or “scissors”): ");
 }
 
-function playRound(humanChoice, computerChoice) {
-    // Rules for a round
-    // 1. Paper beats Rock
-    // 2. Rock beats scissors
-    // 3. Scissors beats Paper
-    // if a tie occurs, retry the round until different results
-
-    let winner;
-    let loser;
-    let state;
-    let score; // true if human wins and false if computer wins
-
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-
-    while (humanChoice == computerChoice) {
-        alert("A tie occured. Press 'OK' to try this round again.")
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-    }
-
-    if (humanChoice == "paper" && computerChoice == "rock" ||
-        humanChoice == "rock" && computerChoice == "scissors" ||
-        humanChoice == "scissors" && computerChoice == "paper"
-    ) {
-        score = true;
-        winner = humanChoice;
-        loser = computerChoice;
-        state = "win";
-    }
-    else  {
-        score = false;
-        winner = computerChoice;
-        loser = humanChoice;
-        state = "lose";
-    }
-
-    console.log(`You ${state}! ${winner.at(0).toUpperCase() + winner.substring(1)} beats ${loser.at(0).toUpperCase() + loser.substring(1)}`);
-    return score;
-}
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        // Rules for a round
+        // 1. Paper beats Rock
+        // 2. Rock beats scissors
+        // 3. Scissors beats Paper
+        // if a tie occurs, retry the round until different results
+
+        let winner;
+        let loser;
+        let state;
+
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        while (humanChoice == computerChoice) {
+            alert("A tie occured. Press 'OK' to try this round again.")
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+        }
+
+        if (humanChoice == "paper" && computerChoice == "rock" ||
+            humanChoice == "rock" && computerChoice == "scissors" ||
+            humanChoice == "scissors" && computerChoice == "paper"
+        ) {
+            humanScore++;
+            winner = humanChoice;
+            loser = computerChoice;
+            state = "win";
+        }
+        else  {
+            computerScore++; 
+            winner = computerChoice;
+            loser = humanChoice;
+            state = "lose";
+        }
+
+        console.log(`You ${state}! ${winner.at(0).toUpperCase() + winner.substring(1)} beats ${loser.at(0).toUpperCase() + loser.substring(1)}`);
+        return score;
+    }
 
     alert("Welcome to a game of 'Rock, Paper, Scissors'! This game would have 5 rounds. Click 'OK' to continue!");
 
     for (let i = 1; i <= 5; i++) {
         console.log("Round " + i);
-        let score = playRound(getHumanChoice(), getComputerChoice());
-        if (score) humanScore++;
-        else computerScore++;        
+        playRound(getHumanChoice(), getComputerChoice());     
     }
 
     let winner, loser, winScore, loseScore;
